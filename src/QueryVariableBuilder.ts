61c6deaa0ef4230105b516f06dc437f411cd75e2
Florent Haxha <kryptag@gmail.com>
2021-12-13 15:17:22 +0100

    added function docs

--- src/QueryVariableBuilder.ts
+++ src/QueryVariableBuilder.ts
@@ -8,12 +8,21 @@ export class QueryVariableBuilder {
     constructor() {
         this.queryVariable = {};
     }
-
+    /**
+     * @param {[String]} gqlInputType example "parentSorInput", "queryInput" ...... 
+     */
     setQueryType(gqlInputType: string) {
         this.queryType = gqlInputType;
         return this;
     }
 
+    /**
+     * @param {[Object][String]} innerQueryVar Can be digested in multiple forms such as 
+     * "{\"SghAfdkode\": \"9704\"}", 
+     * {SghAfdkode: 9704} or 
+     * { "SORkode": "1195041000016003" }
+     * this depends on the SorService Query
+     */
     setQueryVariable(innerQueryVar: {} | string) {
         this.innerQueryVar = innerQueryVar instanceof Object ? JSON.stringify(innerQueryVar) : innerQueryVar;
         this.updateQuery()
@@ -24,7 +33,10 @@ export class QueryVariableBuilder {
         var innerQV = { query: this.innerQueryVar };
         this.queryVariable[this.queryType] = innerQV;
     }
-
+    /**
+     * @param {[any]} varKey any key supported by the query - Example "depth"
+     * @param {[any]} varVal any value supported by the query - Example 2
+     */
     addVariable(varKey: any, varVal: any) {
         var innerQuery = this.queryVariable[this.queryType]
         innerQuery[varKey] = varVal
